6a33a054535b96f5d0c1a3024c0244f388f04e16
WalterCorsini <corsini.walter@gmail.com>
2024-05-21 17:51:16 +0200

    add style

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$count= 0;
-$hotels_filtered=[];
+$count = 0;
+$hotels_filtered = [];
 $hotels = [
 
     [
@@ -60,25 +60,42 @@ $hotels = [
 </head>
 
 <body class="p-5 d-flex justify-content-center align-items-center flex-column">
-    <form class="pb-5" action="index.php" method="POST">
-        <button type="submit">Avvia Ricerca</button>
-        <input id="park" name="park_required" type="checkbox">
-        <label for="park">parcheggio</label>
-        <input id="voto" name="vote" type="number" min="1" max="5" value="1">
-        <label for="voto">voto da 1 a 5</label>
+
+    <!-- form -->
+    <form class="pb-5 w-100 d-flex justify-content-center align-items-center gap-5" action="index.php" method="POST">
+
+        
+        <button type="submit" class="btn btn-outline-success rounded-5">Success</button>
+        
+        <div class="d-flex flex-column justify-content-center align-items-center gap-3">
+            <div>
+            <input class="rounded-5 p-2" id="voto" name="vote" type="number" min="1" max="5" value="1">
+            <label for="voto">voto da 1 a 5</label>
+            </div>
+            <div>
+                <input id="park" name="park_required" type="checkbox">
+                <label for="park">parcheggio</label>
+            </div>
+        </div>
+
 
     </form>
+    <!-- /form -->
 
-        <?php if(isset($_POST['park_required'])){?>
-        <?php $hotels_filtered = array_filter($hotels , function($hotels) { return $hotels["parking"]; }) ; ?>
-        <?php } else{ ?>
+    <?php if (isset($_POST['park_required'])) { ?>
+        <?php $hotels_filtered = array_filter($hotels, function ($hotels) {
+            return $hotels["parking"];
+        }); ?>
+    <?php } else { ?>
         <?php $hotels_filtered = $hotels; ?>
-        <?php } ?>
-        <?php if(isset($_POST["vote"])){?>
-        <?php $hotels_filtered = array_filter($hotels_filtered , function($hotels_filtered) { return $hotels_filtered["vote"] >= $_POST["vote"]; }) ; ?>
-        <?php }?>
+    <?php } ?>
+    <?php if (isset($_POST["vote"])) { ?>
+        <?php $hotels_filtered = array_filter($hotels_filtered, function ($hotels_filtered) {
+            return $hotels_filtered["vote"] >= $_POST["vote"];
+        }); ?>
+    <?php } ?>
 
-        <?php if(count($hotels_filtered)>0){ ?>
+    <?php if (count($hotels_filtered) > 0) { ?>
         <!-- table -->
         <table class="table w-75 m-auto text-center">
             <tbody>
@@ -94,21 +111,27 @@ $hotels = [
                         <?php foreach ($cur_hotel as $key => $value) { ?>
                             <td scope="row">
                                 <!-- check or cross for parking -->
-                                <?php if($value === true){ $value = "<span style=color:green>&check;</span>"; } elseif($value === false) { $value = "<span style=color:red>&cross;</span>"; } ?>
+                                <?php if ($value === true) {
+                                    $value = "<span style=color:green>&check;</span>";
+                                } elseif ($value === false) {
+                                    $value = "<span style=color:red>&cross;</span>";
+                                } ?>
                                 <!-- add "km" at distance -->
-                                <?php if($key === "distance_to_center"){ $value.= " km"; } ?>
-                                <?php if($key === "vote"){
-                                    $copy=$value;
-                                    $value="";
-                                    for($i=0; $i<$copy; $i++){
+                                <?php if ($key === "distance_to_center") {
+                                    $value .= " km";
+                                } ?>
+                                <?php if ($key === "vote") {
+                                    $copy = $value;
+                                    $value = "";
+                                    for ($i = 0; $i < $copy; $i++) {
                                         $value .= "<span class=gold>&#9733;</span>";
                                     }
-                                    for($i=0; $i<5-$copy; $i++){
+                                    for ($i = 0; $i < 5 - $copy; $i++) {
                                         $value .= "&#9734;";
                                     }
                                 }
                                 ?>
-                                
+
                                 <?php echo $value; ?>
                             </td>
                         <?php } ?>
@@ -117,9 +140,9 @@ $hotels = [
             </tbody>
         </table>
         <!-- /table -->
-        <?php } else{ ?>
+    <?php } else { ?>
         <h1 class="pt-5">nessun hotel trovato!</h1>
-        <?php } ?>
+    <?php } ?>
 
 
 </body>
