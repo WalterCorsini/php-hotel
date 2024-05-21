456ff570424ac0d426b457bfdb6d35a704a0c548
WalterCorsini <corsini.walter@gmail.com>
2024-05-21 14:25:09 +0200

    add table

--- index.php
+++ index.php
@@ -43,21 +43,35 @@ $hotels = [
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-hotel</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    <!-- style css -->
     <link rel="stylesheet" href="/css/style.css">
 </head>
+
 <body>
 
-    <?php foreach($hotels as $cur_hotel){ ?>
-        <?php foreach($cur_hotel as $key => $value){ ?>
-            <p>
-                <?php echo "$key : $value"; ?>
-            </p>
-        <?php } ?>
-    <?php } ?>
+    
+    <table class="table">
+        <tbody>
+                <?php foreach ($hotels as $cur_hotel) { ?>
+                <tr>
+                    <?php foreach ($cur_hotel as $key => $value) { ?>
+                        <td scope="row">
+                            <?php echo "$key : $value"; ?>
+                        </td>
+                        <?php } ?>
+                    </tr>
+                    <?php } ?>
+        </tbody>
+    </table>
 
 </body>
+
 </html>
